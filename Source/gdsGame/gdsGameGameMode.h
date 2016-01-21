f62c688a935eb3237bfa31a8665f4535be75d828
Gamereat <djyeats@gmail.com>
2016-01-21 22:23:42 +0000

    Added source

---
+++ Source/gdsGame/gdsGameGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "gdsGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GDSGAME_API AgdsGameGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
