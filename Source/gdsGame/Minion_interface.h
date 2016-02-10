ea95bd88c9856165d17a50b785b4bfcc0f3891be
rizlaking <josh_8d@hotmail.co.uk>
2016-02-10 13:09:19 +0000

    Added Minion Spawning and Tracking

--- Source/gdsGame/Minion_interface.h
+++ Source/gdsGame/Minion_interface.h
@@ -20,6 +20,24 @@ public:
 	// Called every frame
 	virtual void Tick( float DeltaSeconds ) override;
 
-	
+protected:
+	// Spawn a Minion
+	virtual void SpawnMinion(bool player);
+
+	// Update Minions
+	virtual void UpdateMinions();
+
+	/* The Minion that will be spawn */
+	UPROPERTY(EditAnywhere, Category = "Spawning")
+	TSubclassOf<class AMinion> MinionToSpawn;
+
+	// List of player active minions
+	TArray<class AMinion*> player_active_minions;
+
+	// List of ai active minions
+	TArray<class AMinion*> ai_active_minions;
+
+	float timePassed;
+	bool changePlayer;
 	
 };
